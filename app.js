7310a33913f66b6db460f5f262c314f71d42072e
Kevin Jiang <kevin51jiang@email.com>
2019-02-10 00:21:32 -0500

    got bot to recognize roles and not send itself into an infinite loop

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const client = new discord.Client();
 const config = require('./config.json');
 
 client.on('ready', () => {
-    console.log('Logged in as ${client.user.tag}!');
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-    if(message.content.includes("uwu")) {
+    
+    if(message.member.roles.some(role => 
+            role.name === config.roleNames.restricted) 
+        && !message.member.roles.some(role =>
+            role.name === config.roleNames.bot)) {
+
         message.channel.send('Pay ur taxes pleb');
     }
 });
