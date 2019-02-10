9c2129e9b068f256f17484088c6a9d1d8c6732e8
Kevin Jiang <kevin51jiang@email.com>
2019-02-09 23:18:42 -0500

    add eslint; add sample code to app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world my dudes");
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on('ready', () => {
+    console.log('Logged in as ${client.user.tag}!');
+});
+
+client.on('message', msg => {
+    if(msg.includes("uwu")) {
+        msg.reply('Pay ur taxes pleb');
+    }
+});
+
+client.login('NTQ0MDA2NjE3NzAwNDk5NDY2.D0E1uw.5EF7e45UlIDFqvhl2XwvXWYCgGY');
