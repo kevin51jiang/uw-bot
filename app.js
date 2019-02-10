d23196681fdafb5bab8825e05761d31a91a4f22b
Kevin Jiang <kevin51jiang@email.com>
2019-02-09 23:37:49 -0500

    got basic tutorial working (with config.json), plus npm start

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const discord = require('discord.js');
 const client = new discord.Client();
+const config = require('./config.json');
 
 client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}!');
 });
 
-client.on('message', msg => {
-    if(msg.includes("uwu")) {
-        msg.reply('Pay ur taxes pleb');
+client.on('message', message => {
+    if(message.content.includes("uwu")) {
+        message.channel.send('Pay ur taxes pleb');
     }
 });
 
-client.login('NTQ0MDA2NjE3NzAwNDk5NDY2.D0E1uw.5EF7e45UlIDFqvhl2XwvXWYCgGY');
+client.login(config.token);
